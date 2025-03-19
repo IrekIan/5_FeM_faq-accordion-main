7446e92ac6caab4886fae037bf23bb4c2004fa9d
IrekIan <ir.jn.kr@gmail.com>
2025-03-19 21:38:58 +0000

    done

---
+++ index.js
@@ -0,0 +1,16 @@
+document.addEventListener(`click`, function (e) {
+  if (e.target.matches(`.answ-btn`)) {
+    let answBtn = document.querySelector(
+      `div[data-index="${e.target.dataset.index}"]`
+    );
+    answBtn.classList.toggle("answer");
+    let plusImg = document.querySelector(
+      `img[data-index="${e.target.dataset.index}"]`
+    );
+    if (plusImg.src.includes("icon-plus.svg")) {
+      plusImg.src = "assets/images/icon-minus.svg";
+    } else {
+      plusImg.src = "assets/images/icon-plus.svg";
+    }
+  }
+});
